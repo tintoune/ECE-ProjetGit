6037e1c7a9f7130c45125172409ebeada29ac748
leodevin <ldevincre@MacBook-Pro-Leo-2.local>
2018-02-16 12:53:00 +0100

    Je suis le premier a tester

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Cartes.h"
+#include "Creatures.h"
+#include "Energies.h"
+#include "Creatures.h"
+#include "Speciales.h"
+#include <stdlib.h>
+#include "Initialisation.hpp"
+#include "Menu.hpp"
+
+
+
+int main()
+{
+    std::vector<Cartes*>bibli;
+    
+    bibli=init ();
+    
+    int nbLignestot=0;
+    nbLignestot=nblignesfichier()/2;
+    
+//    for (unsigned int i=0; i<nbLignestot ; i++)     //affiche toutes les cartes initialisé à partir du fichier
+//    {
+//        bibli[i]->afficher_info();
+//    }
+    
+    menu();
+    
+    
+    
+    
+    
+    return 0;
+}
